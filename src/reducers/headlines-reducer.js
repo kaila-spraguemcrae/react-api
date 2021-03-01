d1ea31e76f058c4012ba047ef39b88277384e6b4
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-02-28 20:16:14 -0800

    pass headlines-reducer test 3

--- src/reducers/headlines-reducer.js
+++ src/reducers/headlines-reducer.js
@@ -1,16 +1,21 @@
 import * as c from './../actions/ActionTypes';
 
-const defaultState = {
+const initialState = {
   isLoading: false,
   headlines: [],
   error: null
 }
 
-export default (state = defaultState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case c.REQUEST_HEADLINES:
       return Object.assign({}, state, {
         isLoading: true
+      });
+    case c.GET_HEADLINES_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
+        headlines: action.headlines
       });
   default:
     return state; 
